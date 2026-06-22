0f0ab9386554c8d0dabf0164da40e64ab3a25b4c
George <george.valuiskyi@gmail.com>
2026-06-22 10:31:54 +0300

    build: init
    Initial build using maven quickstart archetype.

---
+++ src/test/java/io/github/George_Al3xander/AppTest.java
@@ -0,0 +1,13 @@
+package io.github.George_Al3xander;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
