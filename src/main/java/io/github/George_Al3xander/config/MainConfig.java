fb214295fb37024db0eda6809a8a97749ff36b57
George <george.valuiskyi@gmail.com>
2026-06-30 14:13:30 +0300

    build: jpa & db
    Add shared Spring version property for dependency management.
    Configure JPA, transaction management, and a HikariCP data source.
    Add Hibernate, H2, Spring ORM, and Spring JDBC dependencies.
    Set up an EntityManagerFactory with Hibernate properties for
    database integration.

--- src/main/java/io/github/George_Al3xander/config/MainConfig.java
+++ src/main/java/io/github/George_Al3xander/config/MainConfig.java
@@ -1,15 +1,64 @@
 package io.github.George_Al3xander.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 @ComponentScan(basePackages = "io.github.George_Al3xander")
 @PropertySource("classpath:application.properties")
 public class MainConfig {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:mem:testdb");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        ds.setDriverClassName("org.h2.Driver");
+        return ds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds) {
+
+        LocalContainerEntityManagerFactoryBean emf =
+                new LocalContainerEntityManagerFactoryBean();
+
+        emf.setDataSource(ds);
+        emf.setPackagesToScan("io.github.George_Al3xander");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendorAdapter);
+
+        Properties props = new Properties();
+        props.put("hibernate.hbm2ddl.auto", "update");
+        props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        props.put("hibernate.show_sql", "true");
+
+        emf.setJpaProperties(props);
+
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
     }
 }
