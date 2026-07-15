890af830a6d860bdc448d7bd3118ad2a8ce9cf4f
George <george.valuiskyi@gmail.com>
2026-07-15 12:06:05 +0300

    feat: trainer registration mapping
    Add mapper support for converting trainer registration requests.
    Enables creating Trainer entities from registration DTO data.

--- src/main/java/io/github/George_Al3xander/mapper/TrainerMapper.java
+++ src/main/java/io/github/George_Al3xander/mapper/TrainerMapper.java
@@ -1,5 +1,6 @@
 package io.github.George_Al3xander.mapper;
 
+import io.github.George_Al3xander.dto.trainer.TrainerRegistrationRequest;
 import io.github.George_Al3xander.dto.trainer.TrainerSummaryResponse;
 import io.github.George_Al3xander.model.Trainer;
 import org.mapstruct.Mapper;
@@ -7,4 +8,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface TrainerMapper {
     TrainerSummaryResponse toSummary(Trainer trainer);
+
+    Trainer toTrainer(TrainerRegistrationRequest request);
 }
