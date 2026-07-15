73a28fd671a9deed07645711dba48c25dade5c48
George <george.valuiskyi@gmail.com>
2026-07-15 12:03:43 +0300

    feat: trainer registration request dto
    Create DTO for trainer registration input validation.
    Defines required trainer name and specialization fields.

---
+++ src/main/java/io/github/George_Al3xander/dto/trainer/TrainerRegistrationRequest.java
@@ -0,0 +1,22 @@
+package io.github.George_Al3xander.dto.trainer;
+
+import io.github.George_Al3xander.dto.TrainingTypeResponse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainerRegistrationRequest {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    private TrainingTypeResponse specialization;
+}
