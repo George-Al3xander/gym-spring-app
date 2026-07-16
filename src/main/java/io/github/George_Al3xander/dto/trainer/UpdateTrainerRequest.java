ed87e16fa475a5f2b69902591e6c35924710a8ce
George <george.valuiskyi@gmail.com>
2026-07-16 09:29:08 +0300

    feat: update trainer request dto
    Add request DTO for updating trainer information.
    Include validation for required names and active status.
    Generate constructors and accessors with Lombok.

---
+++ src/main/java/io/github/George_Al3xander/dto/trainer/UpdateTrainerRequest.java
@@ -0,0 +1,21 @@
+package io.github.George_Al3xander.dto.trainer;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTrainerRequest {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    private Boolean isActive;
+}
