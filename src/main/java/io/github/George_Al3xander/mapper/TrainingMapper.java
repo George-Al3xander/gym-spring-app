89808af7722ff2fbd85ef771afebca2a16e96c7b
George <george.valuiskyi@gmail.com>
2026-07-17 16:33:18 +0300

    feat: training response mapper
    Add MapStruct mapper for converting trainings to response DTOs.
    Enable mapping of training entities for trainee responses.

---
+++ src/main/java/io/github/George_Al3xander/mapper/TrainingMapper.java
@@ -0,0 +1,11 @@
+package io.github.George_Al3xander.mapper;
+
+import io.github.George_Al3xander.dto.trainee.TraineeTrainingResponse;
+import io.github.George_Al3xander.model.Training;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TrainingMapper {
+
+    TraineeTrainingResponse toTraineeResponse(Training training);
+}
