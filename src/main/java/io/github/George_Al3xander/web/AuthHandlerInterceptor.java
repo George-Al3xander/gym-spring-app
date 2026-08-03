7d2b3360f2e9a6e7cd435816b3d412f863dc631d
George <george.valuiskyi@gmail.com>
2026-08-03 19:44:15 +0300

    feat: authentication timing metrics
    Add Micrometer timing support for authentication processing.
    Track authentication execution duration using Prometheus metrics.

--- src/main/java/io/github/George_Al3xander/web/AuthHandlerInterceptor.java
+++ src/main/java/io/github/George_Al3xander/web/AuthHandlerInterceptor.java
@@ -3,6 +3,8 @@ package io.github.George_Al3xander.web;
 import io.github.George_Al3xander.dto.auth.CredentialsDTO;
 import io.github.George_Al3xander.exception.BadCredentialsException;
 import io.github.George_Al3xander.service.AuthenticationService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @RequiredArgsConstructor
 public class AuthHandlerInterceptor implements HandlerInterceptor {
     private final AuthenticationService authenticationService;
+    private final MeterRegistry meterRegistry;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -20,6 +23,7 @@ public class AuthHandlerInterceptor implements HandlerInterceptor {
         String method = request.getMethod();
 
         if (uri.contains("api-docs") || uri.contains("swagger") || uri.contains("/actuator")) {
+            //TODO: micrometer
             return true;
         }
 
@@ -37,11 +41,16 @@ public class AuthHandlerInterceptor implements HandlerInterceptor {
     }
 
     private boolean isAuthorized(HttpServletRequest request) {
-        CredentialsDTO credentials = new CredentialsDTO(
-                request.getHeader(AuthHttpHeader.USERNAME),
-                request.getHeader(AuthHttpHeader.PASSWORD)
-        );
+        return Timer.builder("api_methods_authentication_time")
+                .description("Time spent authenticating API requests")
+                .register(meterRegistry)
+                .record(() -> {
+                    CredentialsDTO credentials = new CredentialsDTO(
+                            request.getHeader(AuthHttpHeader.USERNAME),
+                            request.getHeader(AuthHttpHeader.PASSWORD)
+                    );
 
-        return authenticationService.authenticate(credentials);
+                    return authenticationService.authenticate(credentials);
+                });
     }
 }
