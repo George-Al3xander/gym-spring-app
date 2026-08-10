b1d8295f179971567e8f3e693167351cb5092036
George <george.valuiskyi@gmail.com>
2026-08-10 10:52:06 +0300

    feat: token dao
    Add a TokenDao repository with queries for active user tokens and
    token lookup by token value.

---
+++ src/main/java/io/github/George_Al3xander/dao/TokenDao.java
@@ -0,0 +1,15 @@
+package io.github.George_Al3xander.dao;
+
+import io.github.George_Al3xander.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenDao extends JpaRepository<Token, Long> {
+    List<Token> findAllByUserIdAndExpiredFalseAndRevokedFalse(Long userId);
+
+    Optional<Token> findByToken(String token);
+}
