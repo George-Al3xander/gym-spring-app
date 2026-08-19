4ce06fd749f832d1e0ccc7cf84937f211efbefbc
George <george.valuiskyi@gmail.com>
2026-08-19 17:18:06 +0300

    build: initial trainer stats app
    Add the initial build of the trainer stats application.

---
+++ trainer-stats-app/src/main/java/io/github/George_Al3xander/TrainerStatsApplication.java
@@ -0,0 +1,13 @@
+package io.github.George_Al3xander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerStatsApplication.class, args);
+    }
+
+}
---
+++ trainer-stats-app/src/test/java/io/github/George_Al3xander/TrainerStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.George_Al3xander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
