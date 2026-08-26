d0578310cc566eba4638c6d101c2453242a6e93b
George <george.valuiskyi@gmail.com>
2026-08-26 15:52:02 +0300

    feat: feign requests -> propagate bearer token
    Add a Feign interceptor to forward the incoming JWT authorization
    header when making requests to downstream services.

---
+++ gym-app/src/main/java/io/github/George_Al3xander/auth/BearerTokenPropagationInterceptor.java
@@ -0,0 +1,27 @@
+package io.github.George_Al3xander.auth;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class BearerTokenPropagationInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+
+            String authorization = request.getHeader("Authorization");
+
+            if (authorization != null && authorization.startsWith("Bearer ")) {
+                template.header("Authorization", authorization);
+            }
+        }
+    }
+}
